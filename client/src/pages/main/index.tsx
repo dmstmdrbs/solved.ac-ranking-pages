da7cce0a90647fce847948fe3f4660c2763834dc
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-02-27 13:44:49 +0900

    refactor: add key for Profile component in Main page

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -7,29 +7,40 @@ import { Container, Wrapper, Logo } from "./styles";
 import { useEffect, useState } from "react";
 
 const Main = () => {
-  const [memberList, setMemberList] = useState(["maxcha98", "dmstmdrbs", "dre12am14", "tjdqls1668", "tph00300"]);
-  const handleAddMember = (member: string) => setMemberList([...memberList, member]);
+	const [memberList, setMemberList] = useState([
+		"maxcha98",
+		"dmstmdrbs",
+		"dre12am14",
+		"tjdqls1668",
+		"tph00300",
+	]);
+	const handleAddMember = (member: string) =>
+		setMemberList([...memberList, member]);
 
-  return (
-    <Container>
-      <Logo>AJOU</Logo>
-      <Wrapper>
-        {memberList.map((element) => {
-          const { isLoading: userDataLoading, data: userData } = useQuery<IUser>(["user-data", element], () =>
-            fetchUserProfile(element)
-          );
-          if (userDataLoading)
-            return (
-              <div key={element}>
-                <h1>Loading...</h1>
-              </div>
-            );
-          else return userData !== undefined && <Profile {...userData} />;
-        })}
-      </Wrapper>
-      <ManageBoard memberList={memberList} handleAddMember={handleAddMember} />
-    </Container>
-  );
+	return (
+		<Container>
+			<Logo>AJOU</Logo>
+			<Wrapper>
+				{memberList.map((element) => {
+					const { isLoading: userDataLoading, data: userData } =
+						useQuery<IUser>(["user-data", element], () =>
+							fetchUserProfile(element)
+						);
+					if (userDataLoading)
+						return (
+							<div key={element}>
+								<h1>Loading...</h1>
+							</div>
+						);
+					else
+						return (
+							userData !== undefined && <Profile key={element} {...userData} />
+						);
+				})}
+			</Wrapper>
+			<ManageBoard memberList={memberList} handleAddMember={handleAddMember} />
+		</Container>
+	);
 };
 
 export default Main;
